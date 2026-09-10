5f923d45dcf3d99ec662041f851a21111de898d7
YashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2026-09-10 20:18:49 +0530

    Update localStorage.js

--- InterviewQuestions/localStorage.js
+++ InterviewQuestions/localStorage.js
@@ -66,3 +66,29 @@ setItemToLocalStorage("Age", 45);
 console.log(getItemFromLocalStorage("Age"));
 
 localStorage.clear();
+
+/*
+
+Different typs of Storage API via window  object from browser (WEB API's) 
+
+- sessionStorage : per seession data i.e when user closes the tab or the window (browser) the data is removed 
+  unlike cookies these are not sent to server via http request so better 
+  session storage can storeg large amoun t of ddata as compared to cookies 
+  cookies are sent for every htpp request between browser and server 
+
+- localstorage also same as session storagebut it doesnot have expiry it stays even when window or browser closed even if system is shutdoen 
+- localstorage has the memeory capcity allowing to stoire maximum emory datapossible as compared to sessionStorage
+
+- usewr preferences stored inlocalstorage so that easy to pick it up[ from users browser itself rather tahn api call each time 
+- this allows to keep user specific daat in their own browser localstorage 
+
+  localstorage is specific to each orgini 
+  origin = protocol (http/https) + domain + port 
+
+  so https://www.akshaysaini localstorage is different from https://b;log.akshaysiani as domain changed it s a subdomian 
+
+  localstorage is specific to the origin i,e protocol + domain + port 
+
+  This is called same origin policy 
+
+*/
